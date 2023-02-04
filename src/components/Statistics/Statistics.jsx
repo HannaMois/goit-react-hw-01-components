e49018b112fa6d02822a4e423510b65824a86460
HannaMois <hannamois135@gmail.com>
2023-02-04 16:53:28 +0200

    Create components\Statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import {StatBox, Title, StatList, ListLabel, ListPercent} from './Statistics.styled';
+
+function getRandomHexColor() {
+    let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return bgColor;
+  }
+
+  export default function Statistics({ title, stats }) {
+    return (
+      <StatBox>
+        {title && <Title>{title}</Title>}
+  
+        <StatList>
+          {stats.map(el => (
+            <li
+              key={el.id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <ListLabel>{el.label}</ListLabel>
+              <ListPercent>{el.percentage}%</ListPercent>
+            </li>
+          ))}
+        </StatList>
+      </StatBox>
+    );
+  }
+  
+  Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+      PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      }).isRequired
+    ).isRequired,
+  };
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled'
+
+export const StatBox = styled.section`
+    width: 450px;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0px 5px 15px var(--box-shadow);
+    border-radius: 5px;
+`;
+
+export const Title = styled.h2`
+    margin: 0;
+    padding: 20px;
+    text-transform: uppercase;
+    text-align: center;
+    color: var(--main-color);
+    font-weight: bold;
+    font-size: 40px;
+`;
+
+export const StatList = styled.ul`
+    list-style: none;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    box-shadow: 0px 5px 15px var(--box-shadow);
+    margin: 0;
+    padding: 0;
+    
+    li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 5px
+    }
+`;
+
+export const ListLabel = styled.span`
+    padding-top: 20px;
+    font-size: 15px;
+    color: var(--black-color);
+    font-weight: bold;
+`;
+
+export const ListPercent = styled.span`
+    padding: 20px;
+    font-size: 25px;
+    font-weight: bold;
+    color: var(--color-white);
+`;
